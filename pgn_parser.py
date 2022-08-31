a771362c351e2fdc5415f01afb5ca714c023d413
itsf4llofstars <irooted4hal@mailfence.com>
2022-08-31 13:45:18 -0500

    Add check_path_format function

--- pgn_parser.py
+++ pgn_parser.py
@@ -1,5 +1,15 @@
 #!/usr/bin/env python3
+import os
 import re
 
+
+def check_path_format(path, filename):
+    if not path.startswith('/'):
+        path = '/' + path
+    return path
+
+
 if __name__ == "__main__":
-    pass
+    file_path = "home/bumper/python/chess_regex"
+    file_path = check_path_format(file_path, 'nnn')
+    print(file_path)
