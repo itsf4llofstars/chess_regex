c7429019b94fdad052a18a2640d4c1f81cad28b7
itsf4llofstars <irooted4hal@mailfence.com>
2022-08-31 14:03:41 -0500

    Add check_path_filename function

--- pgn_parser.py
+++ pgn_parser.py
@@ -3,13 +3,30 @@ import os
 import re
 
 
-def check_path_format(path, filename):
+def check_path_format(path):
     if not path.startswith('/'):
         path = '/' + path
     return path
 
 
+def check_path_filename(path, filename):
+    path_filename: str = None
+
+    if os.path.isdir(path):
+        path_filename = os.path.join(path, filename)
+
+    if os.path.isfile(path_filename):
+        return path_filename
+
+    return None
+
+
+
 if __name__ == "__main__":
     file_path = "home/bumper/python/chess_regex"
-    file_path = check_path_format(file_path, 'nnn')
-    print(file_path)
+    filename = "test-chess.pgn"
+    file_path = check_path_format(file_path)
+
+    full_path = check_path_filename(file_path, filename)
+    print(full_path)
+
