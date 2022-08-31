ef7ea2d0fa394fa43dc36dcb692bd8a16217444f
itsf4llofstars <irooted4hal@mailfence.com>
2022-08-31 14:18:43 -0500

    Add read_pgn_file function

--- pgn_parser.py
+++ pgn_parser.py
@@ -45,10 +45,31 @@ def check_path_filename(path, filename):
     return None
 
 
+def read_pgn_file(full_path):
+    lines_stripped = []
+
+    try:
+        with open(full_path, 'r') as fo:
+            pgn_file = fo.readlines()
+    except FileNotFoundError as fnfe:
+        print(f"{fnfe}")
+    except Exception as e:
+        print(f"{e}")
+    else:
+        for line in pgn_file:
+            lines_stripped.append(line.strip())
+    finally:
+        if len(lines_stripped):
+            return lines_stripped
+    return None
+
+
 if __name__ == "__main__":
     file_path = "home/bumper/python/chess_regex"
     filename = "test-chess.pgn"
-    file_path = check_path_format(file_path)
 
-    full_path = check_path_filename(file_path, filename)
-    print(full_path)
+    file_path = check_path_format(file_path)
+    path_file = check_path_filename(file_path, filename)
+    chess_pgn = read_pgn_file(path_file)
+
+    [print(line) for line in chess_pgn]
