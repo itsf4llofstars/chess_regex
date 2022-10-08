02cf2c099005288da2b971d74c74bb9b83141bf5
itsf4llofstars <irooted4hal@mailfence.com>
2022-10-08 13:41:09 -0500

    Add white wins regex

---
+++ reg-test.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""This is a test file only, it houses
+the regex data to find certain games
+"""
+import re
+import menu as m
+
+if __name__ == '__main__':
+    pgn_file = []
+    with open('/home/bumper/chess/pgn-one.pgn', 'r') as fo:
+        pgn_data = fo.readlines()
+        for line in pgn_data:
+            pgn_file.append(line.strip())
+
+    white_win = r'(\s1-0)$'
+    for line in pgn_file:
+        if re.search(white_win, line):
+            print(line)
