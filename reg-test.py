e0972a7c10f71285722eaee243f4c1573392ace5
itsf4llofstars <irooted4hal@mailfence.com>
2022-10-08 14:22:34 -0500

    Add white/black ileagal

--- reg-test.py
+++ reg-test.py
@@ -22,13 +22,19 @@ if __name__ == '__main__':
 #         if re.search(black_win, line):
 #             print(line)
 
-    white_mates = r'(#\s1-0)$'
-    for line in pgn_file:
-        if re.search(white_mates, line):
-            print(line)
+#     white_mates = r'(#\s1-0)$'
+#     for line in pgn_file:
+#         if re.search(white_mates, line):
+#             print(line)
+#
+#     black_mates = r'(#\s0-1)$'
+#     for line in pgn_file:
+#         if re.search(black_mates, line):
+#             print(line)
 
-    black_mates = r'(#\s0-1)$'
+    white_illeagal_move = r'^(1\.\s[a-hN][3-4acfh]3?)'
+    black_illeagal_move = r'^(1\.\s[a-hN][3-4acfh]3?\s[a-hN][5-6acfh]6?)'
     for line in pgn_file:
-        if re.search(black_mates, line):
+        if re.search(black_illeagal_move, line):
+        # if re.search(white_illeagal_move, line):
             print(line)
-
