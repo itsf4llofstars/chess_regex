30cc1f85622d5cba692092fc897199b951e1dfb8
itsf4llofstars <irooted4hal@mailfence.com>
2022-10-08 13:50:47 -0500

    Fix re.search string var

--- reg-test.py
+++ reg-test.py
@@ -12,18 +12,18 @@ if __name__ == '__main__':
         for line in pgn_data:
             pgn_file.append(line.strip())
 
-    white_win = r'(\s1-0)$'
-    for line in pgn_file:
-        if re.search(white_win, line):
-            print(line)
-
-    black_win = r'(\s0-1)$'
-    for line in pgn_file:
-        if re.search(black_win, line):
-            print(line)
+#     white_win = r'(\s1-0)$'
+#     for line in pgn_file:
+#         if re.search(white_win, line):
+#             print(line)
+#
+#     black_win = r'(\s0-1)$'
+#     for line in pgn_file:
+#         if re.search(black_win, line):
+#             print(line)
 
     white_mates = r'(#\s1-0)$'
     for line in pgn_file:
-        if re.search(black_win, line):
+        if re.search(white_mates, line):
             print(line)
 
