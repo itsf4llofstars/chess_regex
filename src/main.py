07f4e7d98f27b719a2a391a92b56e7bf43dff7e9
itsf4llofstars <irooted4hal@mailfence.com>
2022-10-20 22:03:07 -0500

    Re-Establish all file in src.
    DO NOT MERGE the bacon branch

--- src/main.py
+++ src/main.py
@@ -1,38 +1,12 @@
 #!/usr/bin/env python3
-"""Chess Regex Project main Python3 file"""
-import menu as m
+"""
+This is a re-write of the project on branch bacon et. al.
+bacon is NOT to be merged with main or any other branch
+DO NOT MERGE
+"""
 
-# Needed for F5 run of python script in Vim
-# os.system('clear')
-# os.system('clear')
-#
-# pgn_directory = ''  # chess
-# good_dir: bool = False
-#
-# while not good_dir:
-#     pgn_directory = m.get_pgn_dir()
-#
-#     if not m.check_dir(pgn_directory):
-#         os.system('clear')
-#         print('Unable to find that path.\n')
-#         continue
-#     good_dir = True
-#
-# del good_dir
-#
-# pgn_file_name: str = ''  # pgn-one.pgn
-# good_file = False
-#
-# while not good_file:
-#     pgn_file_name = m.get_pgn_file()
-#
-#     if not m.check_file_exists(pgn_directory, pgn_file_name):
-#         os.system('clear')
-#         print('Unable to find that file.\n')
-#         continue
-#     good_file = True
-#
-# del good_file
-#
-user_option = m.display_options()
-print(f'You Picked: {user_option}')
+
+if __name__ == '__main__':
+    pass
+
+# DO NOT MERGE
--- src/menu.py
+++ src/menu.py
@@ -1,71 +1,11 @@
-#!/usr/bin/env python3.9
-"""menu.py file for selecting pgn file actions"""
-import os
-
-choice = ''
-
-
-def get_pgn_dir():
-    print('Enter the directory of your chess pgn file:')
-    print("If you directory is in '/home/$USER/chess/pgn_files', Enter: chess/pgn_files\n")
-    return str(input())
-
-
-def check_dir(directory: str) -> bool:
-    return os.path.isdir(os.path.expanduser(os.path.join('~', directory)))
-
-
-def get_pgn_file() -> str:
-    print('Enter the name of the chess pgn file. .pgn is not required:')
-    print('Example: chess-games[.pgn]\n')
-    file_name: str = str(input())
-
-    if not file_name.endswith('.pgn'):
-        file_name += '.pgn'
-
-    return file_name
-
-
-def check_file_exists(directory: str, pgn_file: str) -> bool:
-    return os.path.isfile(os.path.expanduser(os.path.join('~', directory, pgn_file)))
-
-
-def display_options():
-    global choice
-
-    options = (
-        '\n\tEnter the number for the option you would like to perform:\n\n'
-        '\tReturn Games that:\n'
-        '\t1) end in a draw\n'
-        '\t2) White wins\n'
-        '\t3) Black wins\n'
-        '\t4) White Checkmates\n'
-        '\t5) Black Checkmates'
-    )
-    os.system('clear')
-    while True:
-        print(options)
-        try:
-            choice = int(input('\n\tChoice: '))
-        except ValueError:
-            os.system('clear')
-            print('\n\tPlease enter a number between 1 and 5.')
-        else:
-            if 1 <= choice < 6:
-                return choice
-            else:
-                os.system('clear')
-                print('\n\tPlease enter a number between 1 and 5.')
-                continue
-
-
-def max_moves():
-    os.system('clear')
-
-    print("Enter the maximum number of game moves in ten's [4 returns games with 39-moves and less]\n")
-    return int(input('What is the maximum amount of moves you would like for your games: '))
+"""
+This is a re-write of the project on branch bacon et. al.
+bacon is NOT to be merged with main or any other branch
+DO NOT MERGE
+"""
 
 
 if __name__ == '__main__':
-    user_choice = display_options()
-    print(user_choice)
+    pass
+
+# DO NOT MERGE
