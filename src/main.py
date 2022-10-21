05f11de4e6981386d9145d33fad6c7cc907fd322
itsf4llofstars <irooted4hal@mailfence.com>
2022-10-20 23:39:12 -0500

    Write get_only_games
    return only legal started games
    
    DO NOT MERGE the bacon branch

--- src/main.py
+++ src/main.py
@@ -8,19 +8,24 @@ import menu as mu
 import pgn_parser as pgn
 import os
 
+
 if __name__ == '__main__':
+    opera = ['1. e4 e5 2. Qf3', '1. e4 e5 2. Kf3', '1. e4 e5 2. f3']
+
     os.system('clear')
 
-    mu.print_menu()
-    users_choice: int = mu.get_user_choice()
-    mu.check_choice(users_choice)
+    # mu.print_menu()
+    # users_choice: int = mu.get_user_choice()
+    # mu.check_choice(users_choice)
 
-    pgn_path: str = pgn.get_path()
-    full_path: str = pgn.expand_path(pgn_path)
+    # pgn_path: str = pgn.get_path()
+    # full_path: str = pgn.expand_path(pgn_path)
 
-    full_path_file: str = os.path.join(full_path, pgn.get_pgn_file())
+    # full_path_file: str = os.path.join(full_path, pgn.get_pgn_file())
+    full_path_file: str = os.path.join('/home/bumper/chess', 'bumper.pgn')
     pgn.check_path_file(full_path_file)
-    print(full_path_file)
+
+    pgn.get_only_games(opera)
 
     print('\n\n\tfin')
 
