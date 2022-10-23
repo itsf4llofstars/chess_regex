e62a27c96d517e12459ada3d8b442dc0454e3964
itsf4llofstars <irooted4hal@mailfence.com>
2022-10-23 00:49:12 -0500

    Add write file fucntion

--- src/file_ops.py
+++ src/file_ops.py
@@ -18,10 +18,19 @@ def read_file(path_file: str):
                 pgn_games.append(line.strip())
     finally:
         if len(pgn_games):
-            [print(game) for game in pgn_games]
+            return pgn_games
+        [print(game) for game in pgn_games]
+
+
+def write_file(path_file: str, chess_games_write):
+    with open(path_file, 'w') as fo:
+        for line in chess_games:
+            fo.write(line)
+            fo.write('\n')
 
 
 if __name__ == '__main__':
     # read_file('/home/bumper/chess/pgn-one.pgn')
-    read_file('/home/bumper/chess/bumper.pgn')
+    chess_games = read_file('/home/bumper/chess/games1.pgn')
+    write_file('/home/bumper/python/chess_regex/src/test_games.pgn', chess_games)
 
