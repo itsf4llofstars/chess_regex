d987a9eba9be68462fcdc6784732e81dc90c6d28
itsf4llofstars <irooted4hal@mailfence.com>
2022-10-26 23:58:04 -0500

    Add function calls

--- src/main.py
+++ src/main.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 """Chess Regex Project main Python3 file"""
-import menu as m
+import menu
 
-users_choice = m.display_options()
-print(users_choice)
+users_choice = menu.display_options()
+pgn_path = menu.get_pgn_dir()
+pgn_path_file = menu.get_pgn_file(pgn_path)
 
 print('fin')
+
