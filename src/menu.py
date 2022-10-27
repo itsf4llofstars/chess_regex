6bb1fde5de964e34389db4086ae70b8b11542de4
itsf4llofstars <irooted4hal@mailfence.com>
2022-10-26 23:51:31 -0500

    Remove tilda in join function

--- src/menu.py
+++ src/menu.py
@@ -76,7 +76,7 @@ def get_pgn_file(directory: str) -> str:
         else:
             break
 
-    return os.path.join('~', directory, file_name)
+    return os.path.join(directory, file_name)
 
 
 def check_file_exists(directory: str, pgn_file: str) -> bool:
