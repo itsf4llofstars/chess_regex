27249eedbf401f791b9f85669fd09211c93cffef
itsf4llofstars <irooted4hal@mailfence.com>
2022-10-28 14:31:14 -0500

    Change input var name to file_name
    Change return to string of path and filename joined

--- tests/test_menu.py
+++ tests/test_menu.py
@@ -5,15 +5,9 @@ from src.menu import check_file_exists
 
 class TestMenu(unittest.TestCase):
     def test_check_dir(self):
-        """Test if True is returned on a known
-        directory
-        """
         test_directory = check_dir('chess_games_dir')
         self.assertEqual(test_directory, '/home/bumper/chess_games_dir')
 
     def test_check_file_exists(self):
-        """Checks if True is return on the existance of
-        a file
-        """
         test_file = check_file_exists('python/chess_regex', 'README.md')
-        self.assertEqual(test_file, True)
+        self.assertEqual(test_file, '/home/bumper/python/chess_regex/README.md')
