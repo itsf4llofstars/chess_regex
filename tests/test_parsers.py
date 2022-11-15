90f080890cde83ebc74a65494cb4212ca21d90b3
itsf4llofstars <irooted4hal@mailfence.com>
2022-11-15 01:11:09 -0600

    Add kibitz list

--- tests/test_parsers.py
+++ tests/test_parsers.py
@@ -36,7 +36,7 @@ winner_test_list = [
 
 winner_test_list1 = [
     "1. c4 e5 2. Nf3 d6 12. d4 1-0",
-    "1. e4 e5, 2. Nf3 d6 2. d4 0-1"
+    "1. e4 e5 2. Nf3 d6 2. d4 0-1"
 ]
 
 strip_mates_test_list = [
@@ -52,7 +52,14 @@ strip_mates_test_list = [
     "1. e4 e5 2. Nf3 d6 31. d4# 1/2-1/2"
 ]
 
-# NOTE: These test are note exhaustive
+kibitz_games = [
+    "1. c4 e5 2. Nf3 d6 12. d4 1-0",
+    "1. e4 e5 { This is kibitz } 2. Nf3 d6 2. d4 0-1"
+    "1. c4 e5 2. Nf3 d6 12. d4 1-0",
+    "1. e4 e5 2. Nf3 ( 2. Nc3 g6 ) 2... d6 2. d4 0-1"
+]
+
+# NOTE: These test are not exhaustive
 
 
 class TestGamesList(unittest.TestCase):
