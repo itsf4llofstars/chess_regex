985caf271d8838989298291bc2da958dbd9d0b62
itsf4llofstars <irooted4hal@mailfence.com>
2022-11-16 14:31:26 -0600

    Setup main, and menu to work on max move functions

--- src/main.py
+++ src/main.py
@@ -7,22 +7,27 @@ from sys import exit
 
 """Call the menu.py display options function to get
 the users game return choice."""
-users_choice = menu.display_options()
+# users_choice = menu.display_options()
+users_choice = 2
 
 """Call menu.py get_pgn_dir function the get the users
 director[y|ies] where the pgn files are"""
-pgn_path = menu.get_pgn_dir()
+# pgn_path = menu.get_pgn_dir()
+pgn_path = 'chess'
 
 """Call menu.py get_pgn_file, passing the path variable
 to get the users pgn filename and join it with the"""
 # path to the pgn file
-pgn_path_file = menu.get_pgn_file(pgn_path)
+# pgn_path_file = menu.get_pgn_file(pgn_path)
+pgn_path_file = '/home/bumper/chess/opera.pgn'
 
 """Call the menu.py max_moves function to get the maximum
 number of moves the user wants for the their game
 These max moves are not currently
 in use."""
 maximum_moves = menu.max_moves()
+print(f'{maximum_moves = }')
+exit()
 
 """Set the maximum_moves variable for numeric validity
 and return it as a string for use in the regex dict in
