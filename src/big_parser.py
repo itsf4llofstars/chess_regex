06c5213684d3a2a04ae0598a436aa6d5fa16d56d
itsf4llofstars <irooted4hal@mailfence.com>
2022-11-25 01:41:03 -0600

    Add regex_dict

--- src/big_parser.py
+++ src/big_parser.py
@@ -2,6 +2,21 @@
 Initial write of big_parser.py the file that parses
 all user selects in one function
 """
+import re
+
+regex_dict = {
+    'legal_start': re.compile(r'^(1\.\s[a-hN][3-4acfh]3?\s[a-hN][5-6acfh]6?)'),
+    'max_move': re.compile(r'\s[4-9]\d\.\s'),
+    'max_moves': '',  #! This is set from menu.py
+    'no_hundred': re.compile(r'\s\d{3}\.\s'),
+    'white_wins': re.compile(r'(\s1-0)$'),
+    'black_wins': re.compile(r'(\s0-1)$'),
+    'white_mates': re.compile(r'(#\s1-0)$'),
+    'black_mates': re.compile(r'(#\s0-1)$'),
+    'kibitz': re.compile(r'\s[(|{]'),
+    'strip_white_mate': re.compile(r'\s\d{1,2}\.\s\w+=?[B-R]?#\s1-0'),
+    'strip_black_mate': re.compile(r'\s\w+=?[B-R]?#\s0-1'),
+}
 
 
 def main():
