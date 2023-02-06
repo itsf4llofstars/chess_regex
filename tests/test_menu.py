831b0e059a9073a7264f73ff08a6049d47156c65
itsf4llofstars <irooted4hal@mailfence.com>
2023-02-05 22:26:41 -0600

    Add os..expanderuser()
    Use now will aid in testing for all linux users

--- tests/test_menu.py
+++ tests/test_menu.py
@@ -3,6 +3,7 @@ NOTE: In order to run your own tests you will need to change
 the self.assertEqual(test_directory, 'CHANGE_THIS_PATH')
 line to a valid directory on your computer
 """
+import os.path
 import unittest
 from src.menu import check_dir
 from src.menu import check_file_exists
@@ -14,5 +15,6 @@ class TestMenu(unittest.TestCase):
         self.assertEqual(test_directory, '/home/bumper/chess_games_dir')
 
     def test_check_file_exists(self):
-        test_file = check_file_exists('/home/bumper/python/chess_regex', 'README.md')
-        self.assertEqual(test_file, '/home/bumper/python/chess_regex/README.md')
+        test_directory = os.path.expanduser(os.path.join('~', 'python/chess_regex'))
+        test_file = check_file_exists(test_directory, 'README.md')
+        self.assertEqual(test_file, f'{test_directory}/README.md')
