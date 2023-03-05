611815044f411aec84e34c77cc17ec8a8f4d2b0e
itsf4llofstars <irooted4hal@mailfence.com>
2023-03-05 01:33:18 -0600

    Add legal start regex search

--- src/big_parser.py
+++ src/big_parser.py
@@ -1,9 +1,15 @@
 # Initial write of big_parser.py the file that parses
 # all user selects in one function
 import os
+import re
 
 file_name = os.path.expanduser(os.path.join('~', 'chess', 'bumper.pgn'))
+min_move = ' 10. '
 max_move = ' 40. '
+
+# Second white move not tested
+legal_start = re.compile(r'^(1\.\s[a-hN][3-4acfh]3?\s[a-hN][5-6acfh]6?\s2\.\s[a-hBNKQR][1-5a-h])')
+
 games = []
 
 with open(file_name, 'r') as fo:
@@ -12,12 +18,15 @@ with open(file_name, 'r') as fo:
         if (
                 not line.startswith('1. ')
                 or not line[-2] == '-'
+                or not min_move in line
                 or '(' in line
                      or ')' in line
                 or '{' in line
                      or '}' in line
                 or max_move in line
                 ):
+            continue
+        elif not re.search(legal_start, line):
             continue
         line = line.replace('!', '')
         line = line.replace('!!', '')
@@ -26,3 +35,7 @@ with open(file_name, 'r') as fo:
         line = line.replace('?!', '')
         line = line.replace('!?', '')
         line = line.replace('+', '')
+        games.append(line)
+
+[print(game) for game in games]
+print(len(games))
