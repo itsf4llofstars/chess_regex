99653d6647f7ed61198077f875389c9248c07c4a
itsf4llofstars <irooted4hal@mailfence.com>
2023-03-05 01:23:06 -0600

    Add replace annotations

--- src/big_parser.py
+++ src/big_parser.py
@@ -1,13 +1,28 @@
 # Initial write of big_parser.py the file that parses
 # all user selects in one function
+import os
 
+file_name = os.path.expanduser(os.path.join('~', 'chess', 'bumper.pgn'))
 max_move = ' 40. '
+games = []
 
 with open(file_name, 'r') as fo:
     for line in fo:
         line = line.strip()
-        if not line.startswith('1. ') or not line[-2] == '-' \
-                or '(' in line or ')' in line \
-                or '{' in line or '}' in line \
-                or max_move in line:
-                    continue
+        if (
+                not line.startswith('1. ')
+                or not line[-2] == '-'
+                or '(' in line
+                     or ')' in line
+                or '{' in line
+                     or '}' in line
+                or max_move in line
+                ):
+            continue
+        line = line.replace('!', '')
+        line = line.replace('!!', '')
+        line = line.replace('?', '')
+        line = line.replace('??', '')
+        line = line.replace('?!', '')
+        line = line.replace('!?', '')
+        line = line.replace('+', '')
