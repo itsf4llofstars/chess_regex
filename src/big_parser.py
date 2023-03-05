14aa131c103583d5bb7ea7a7e30b97498373a2d3
itsf4llofstars <irooted4hal@mailfence.com>
2023-03-05 01:17:10 -0600

    Remove function call

--- src/big_parser.py
+++ src/big_parser.py
@@ -1,10 +1,13 @@
 # Initial write of big_parser.py the file that parses
 # all user selects in one function
 
+max_move = ' 40. '
 
-def parse_chess(file_name):
-    with open(file_name, 'r') as fo:
-        for line in fo:
-            line = line.strip()
-            if not line.startswith('1. ') or '\(' in line:
-                continue
+with open(file_name, 'r') as fo:
+    for line in fo:
+        line = line.strip()
+        if not line.startswith('1. ') or not line[-2] == '-' \
+                or '(' in line or ')' in line \
+                or '{' in line or '}' in line \
+                or max_move in line:
+                    continue
