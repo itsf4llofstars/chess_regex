f74f21764224d66f55eb7d18f0578d9ccd6e2b9f
itsf4llofstars <irooted4hal@mailfence.com>
2023-05-21 23:01:25 -0500

    Add read in file and parse game starts

--- src/main.py
+++ src/main.py
@@ -2,12 +2,31 @@
 """main.py
 This file on the fastest git branch is
 explained in the fastest_branch.md file
+
+Straight through. Little regex as possible.
+Full File read into list, appending to a new
+list and deleting the old as we go.
+pgn file size is 1GB, with approx 500,000+ chess games
+read form a 32GB micro-SD card.
 """
+import os
 
+file_name = "lichess_short.pgn"
+pgn_file = os.path.expanduser(
+    os.path.join("/media", "bumper", "EDD2-E40F", "raspi32", file_name)
+)
 
-def main():
-    pass
+# Read in the file
+pgn_lines = []
+with open(pgn_file) as read:
+    pgn_lines = read.readlines()
 
+# Get only those games starting with 1._
+games = []
+for line in pgn_lines:
+    if line.startswith("1. "):
+        games.append(line.rstrip())
 
-if __name__ == "__main__":
-    main()
+del pgn_lines
+
+[print(game) for game in games]
